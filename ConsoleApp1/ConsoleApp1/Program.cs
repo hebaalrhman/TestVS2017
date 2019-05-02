de67c0e66c222866aff57567ebffe6a88de7448a
hebaalrhman <39726950+hebaalrhman@users.noreply.github.com>
2019-05-02 13:25:36 +0200

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("hello world");
+            Console.WriteLine("hello world from github online");
         }
     }
 }
